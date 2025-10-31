a23fe914731835595d9fc47268fc0962d02d2289
Lisandro Pin <lisandro.pin@proton.ch>
2025-10-31 17:23:37 +0100

    Rework collection resultion for `ec.rebuild`, in preparation for parallelization.

--- weed/shell/command_ec_rebuild.go
+++ weed/shell/command_ec_rebuild.go
@@ -79,20 +79,21 @@ func (c *commandEcRebuild) Do(args []string, commandEnv *CommandEnv, writer io.W
 		return err
 	}
 
+	var collections []string
 	if *collection == "EACH_COLLECTION" {
-		collections, err := ListCollectionNames(commandEnv, false, true)
+		cs, err := ListCollectionNames(commandEnv, false, true)
 		if err != nil {
 			return err
 		}
-		fmt.Printf("rebuildEcVolumes collections %+v\n", len(collections))
-		for _, c := range collections {
-			fmt.Printf("rebuildEcVolumes collection %+v\n", c)
-			if err = rebuildEcVolumes(commandEnv, allEcNodes, c, writer, *applyChanges); err != nil {
-				return err
-			}
-		}
+		collections = cs
 	} else {
-		if err = rebuildEcVolumes(commandEnv, allEcNodes, *collection, writer, *applyChanges); err != nil {
+		collections = []string{*collection}
+	}
+
+	fmt.Printf("rebuildEcVolumes for %+v collection(s)\n", len(collections))
+	for _, c := range collections {
+		fmt.Printf("rebuildEcVolumes collection %+v\n", c)
+		if err = rebuildEcVolumes(commandEnv, allEcNodes, c, writer, *applyChanges); err != nil {
 			return err
 		}
 	}
@@ -101,7 +102,6 @@ func (c *commandEcRebuild) Do(args []string, commandEnv *CommandEnv, writer io.W
 }
 
 func rebuildEcVolumes(commandEnv *CommandEnv, allEcNodes []*EcNode, collection string, writer io.Writer, applyChanges bool) error {
-
 	fmt.Printf("rebuildEcVolumes %s\n", collection)
 
 	// collect vid => each shard locations, similar to ecShardMap in topology.go
